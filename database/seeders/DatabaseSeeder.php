205f4c54a8b5d8d37f1f894853bc5547c3050726
Jonathan <jonathan.oeijoeng@gmail.com>
2026-04-15 21:34:05 +0700

    Fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        User::create([
             'name' => 'Jonathan',
             'email' => 'jonathan.oeijoeng@gmail.com',
         ]);
