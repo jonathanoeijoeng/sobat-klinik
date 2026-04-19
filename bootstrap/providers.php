adce0b765596e69d002f2572ed047c73bec106a2
Jonathan <jonathan.oeijoeng@gmail.com>
2026-04-19 07:55:44 +0700

    Fix error Modal not open

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\FortifyServiceProvider;
+use Livewire\LivewireServiceProvider;
 
 return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
+    LivewireServiceProvider::class,
 ];
